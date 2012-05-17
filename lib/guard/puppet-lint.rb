d65892351c66cadeb90ab381fe16b7937f80b81a
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-05-17 13:18:47 +0400

    make popups pop

--- lib/guard/puppet-lint.rb
+++ lib/guard/puppet-lint.rb
@@ -1,11 +1,28 @@
 require 'guard'
 require 'guard/guard'
 require 'guard/watcher'
+require 'puppet-lint'
+
+class PuppetLint
+
+  attr_reader :messages
+
+  def clear_messages
+    @messages = []
+  end
+
+  def format_message(message)
+    @messages << (log_format % message)
+  end
+end
 
 module Guard
   class Puppetlint < Guard
 
-    VERSION = '0.0.1'
+    def initialize(watchers = [], options = {})
+      @linter = PuppetLint.new
+      super
+    end
 
     # Calls #run_all if the :all_on_start option is present.
     def start
@@ -19,15 +36,15 @@ module Guard
 
     # Print the result of the command(s), if there are results to be printed.
     def run_on_change(res)
-      puts res if res
-    end
-
-  end
-
-  class Dsl
-    # Easy method to display a notification
-    def n(msg, title='', image=nil)
-      Notifier.notify(msg, :title => title, :image => image)
+      messages = []
+      res.each do |file|
+        @linter.file = file
+        @linter.clear_messages
+        @linter.run
+        linter_msg = @linter.messages.reject { |s| !options[:show_warnings] && s =~ /WARNING/ }
+        messages += linter_msg.map {|x| "#{file}: #{x}"} if linter_msg
+      end
+      Notifier.notify( messages.join("\n"), :title => "Puppet lint", :image => :failed )
     end
   end
 end
